1312bfe4b18411df535e1d15ea71f87363785cd0
Richa Yadav <richayadav8525@gmail.com>
2026-07-26 14:55:19 +0530

    Add Project Files

---
+++ init/index.js
@@ -0,0 +1,31 @@
+if (process.env.NODE_ENV != "production") {
+    require("dotenv").config();
+}
+
+const mongoose =require("mongoose");
+const initData=require("./data.js");
+const Listing=require("../models/listing.js");
+const dbUrl = process.env.ATLASDB_URL;
+
+main()
+.then(() => {
+console.log("connected to DB");
+})
+.catch((err)=>{
+    console.log(err);
+});
+
+async function main(){
+    await mongoose.connect(dbUrl);
+}
+
+const initDB= async () =>
+{
+    await Listing.deleteMany({});
+    initData.data = initData.data.map((obj)=>({
+    ...obj,owner:"6a55dc49060bf1a3c0441f04"
+    }));
+    await Listing.insertMany(initData.data);
+    console.log("data was initialized");
+};
+initDB();
---
+++ routes/listing.js
@@ -0,0 +1,57 @@
+const express= require("express");
+const router = express.Router();
+const wrapAsync=require("../utils/wrapAsync.js");
+const Listing = require ("../models/listing.js");
+const {isLoggedIn, isOwner,validateListing} = require("../middleware.js");
+
+const listingController=require("../controllers/listings.js");
+const multer  = require('multer');
+const {storage}=require("../cloudConfig.js");
+const upload = multer({ storage });
+
+router.route("/")
+.get(wrapAsync(listingController.index))
+.post(
+    isLoggedIn,
+  
+    upload.single('listing[image]'),
+      validateListing,
+    wrapAsync(listingController.createListing)
+);
+
+        
+    //New Route
+    router.get("/new",isLoggedIn,listingController.renderNewForm);
+
+    //Show route 
+    router.get("/:id", wrapAsync(listingController.showListing));
+
+
+         //Edit Route
+        router.get("/:id/edit",
+            isLoggedIn,
+              isOwner,
+            wrapAsync(listingController.renderEditForm));
+
+
+        //Update Route
+        router.put(
+            "/:id",
+            isLoggedIn,
+            isOwner,
+             upload.single('listing[image]'),
+            validateListing,
+            wrapAsync(listingController.updateListing)
+        );
+        
+      
+    
+        //Delete Route
+        router.delete("/:id",
+            isLoggedIn,
+              isOwner,
+              wrapAsync(listingController.destroyListing)
+           );
+
+        module.exports = router;
+    
